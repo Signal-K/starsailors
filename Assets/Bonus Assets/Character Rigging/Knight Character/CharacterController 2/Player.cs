33c4644866f618e9fdce3ba0a4a5bcc135706fd3
Gizmotronn <droidology@allianceofdroids.org.au>
2022-07-19 02:47:18 +0800

    🧀🍭 -> Updated shader settings for [SCD-2 SCD-3], will fix biomes next

--- Assets/Bonus Assets/Character Rigging/Knight Character/CharacterController 2/Player.cs
+++ Assets/Bonus Assets/Character Rigging/Knight Character/CharacterController 2/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -29,4 +29,4 @@ public class Player : MonoBehaviour {
     public void GatherMaterial() {
         questCurrentAmount++;
     }
-}
+}*/
