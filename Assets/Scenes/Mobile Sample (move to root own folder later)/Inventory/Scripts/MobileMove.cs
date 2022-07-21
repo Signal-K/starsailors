cd96c589cbe2ff4d8ee60190011bd679936b68ce
Gizmotronn <droidology@allianceofdroids.org.au>
2022-07-22 01:59:40 +0800

    🦔🧪 -> Adding some mobile configuration and starting to revamp RPG style

---
+++ Assets/Scenes/Mobile Sample (move to root own folder later)/Inventory/Scripts/MobileMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileMove : MonoBehaviour {
+    void Update() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            touch.position
+        }
+    }
+}
