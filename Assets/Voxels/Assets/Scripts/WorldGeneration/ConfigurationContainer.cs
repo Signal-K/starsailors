f375edc8540599ae82eed8564d434827aed82b60
Gizmotronn <droidology@allianceofdroids.org.au>
2022-07-21 22:18:09 +0800

    🧊⛓ -> Setting up polybrush for [SCD-3]

--- Assets/Voxels/Assets/Scripts/WorldGeneration/ConfigurationContainer.cs
+++ Assets/Voxels/Assets/Scripts/WorldGeneration/ConfigurationContainer.cs
@@ -10,4 +10,13 @@ public class ConfigurationContainer {
     private Transform parent; // game object that all functions are instantiated to
     [SerializeField]
     private MapElement[] prefabs;
+    [SerializeField]
+    private Texture2D mapData;
+
+    // Dictionary -> all map elements
+    public Dictionary<string, MapElement > MapElements { get; set; } = new Dictionary<string, MapElement>();
+
+    public MapElement[] Prefabs { get => prefabs; private set => prefabs = value; }
+    public Transform Parent { get => parent; private set => parent = value; }
+    public Texture2D MapData { get => mapData; private set => mapData = value; }
 }
--- Assets/Voxels/Assets/Scripts/WorldGeneration/MapElement.cs
+++ Assets/Voxels/Assets/Scripts/WorldGeneration/MapElement.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapElement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[System.Serializable]
+public class MapElement {
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private GameObject[] gameObjects;
+    [SerializeField]
+    private Color32 color;
+    [SerializeField]
+    private MeshRenderer meshRenderer; // determine the size of the tile so we can place the next one
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject[] GameObjects { get => gameObjects; set => gameObjects = value; }
+    public Color32 Color { get => color; set => color = value; }
+    public MeshRenderer MeshRenderer { get => meshRenderer; set => meshRenderer = value; }
 }
