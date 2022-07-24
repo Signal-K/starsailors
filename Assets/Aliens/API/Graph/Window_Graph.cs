02d952321502da140561df0023be128b70d3608e
Gizmotronn <droidology@allianceofdroids.org.au>
2022-07-24 21:20:25 +0800

    🩸🍆 -> Add graph component for [SCD-3] api retrieval

---
+++ Assets/Aliens/API/Graph/Window_Graph.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using CodeMonkey.utils;
+
+public class Window_Graph : MonoBehaviour
+{
+    [SerializeField] private Sprite circleSprite;
+    private RectTransform graphContainer;
+    
+    private void Awake() {
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        //CreateCircle(new Vector2(200, 200));
+        List<int> valueList = new List<int>() { 5, 98, 56, 45, 30, 22, 17, 15, 13, 17, 25, 37, 40, 36, 33 };
+        ShowGraph(valueList);
+    }
+
+    private GameObject CreateCircle(Vector2 anchoredPosition) {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        return gameObject;
+    }
+
+    private void ShowGraph(List<int> valueList) {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 100f; // top of graph
+        float xSize = 50f; // Size distance between each point on x-axis
+        GameObject lastCircleGameObject = null;
+        for (int i = 0; i < valueList.Count; i++) {
+            float xPosition = xSize + i * xSize;
+            float yPosition = (valueList[i] / yMaximum) * graphHeight;
+            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+            if (lastCircleGameObject != null) {
+                CreateDotConnection(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition, circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+            }
+            lastCircleGameObject = circleGameObject;
+        }
+    }
+
+    private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB) { // Create lines/connections between each point on the graph
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance, 3f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        //rectTransform.localEulerAngles = new Vector3(0, 0, UtilsClass.GetAngleFromVectorFloat(dir));
+    }
+}
---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour {
+    public float mouseSensetivity = 100f; // mouse input is multiplied by the sensitivity (can be set in game)
+    float xRotation = 0f; // controlling rotation around the x axis (camera view rotate)
+    float yRotation = 0f;
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked; // Lock cursor to middle of the screen
+    }
+
+    void Update() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensetivity * Time.deltaTime; // input of the x axis of the mouse, configured slightly based on params
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensetivity * Time.deltaTime; // deltaTime -> input based on time since last frame
+        xRotation -= mouseY; // control rotation around x axis (up and down)
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        yRotation += mouseX; // control rotation around y axis -> look up and down
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
