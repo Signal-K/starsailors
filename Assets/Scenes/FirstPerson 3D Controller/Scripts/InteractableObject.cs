931b58797dea61c1b74e2ed8f863f7e850f2880e
Gizmotronn <droidology@allianceofdroids.org.au>
2022-07-30 14:19:36 +0800

    ☔️☃️ -> Add animal/alien prefab for [SCD-4] and documented selection manager
    Interactable object documentation: https://skinetics.notion.site/Interactable-Objects-851b3d1289984b769dd01f0a17c244af

--- Assets/Scenes/FirstPerson 3D Controller/Scripts/InteractableObject.cs
+++ Assets/Scenes/FirstPerson 3D Controller/Scripts/InteractableObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This can be placed on gameObjects or prefabs
 public class InteractableObject : MonoBehaviour {
     public string ItemName;
     public string GetItemName() {
--- Assets/Scenes/FirstPerson 3D Controller/Scripts/SelectionManager.cs
+++ Assets/Scenes/FirstPerson 3D Controller/Scripts/SelectionManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
  
-public class SelectionManager : MonoBehaviour {
+public class SelectionManager : MonoBehaviour { // Interactable object documentation: https://skinetics.notion.site/Interactable-Objects-851b3d1289984b769dd01f0a17c244af
     public GameObject interaction_Info_UI; // referenced in the inspector
     Text interaction_text; // reference to the text component of the GameObject ^^
  
@@ -12,15 +12,15 @@ public class SelectionManager : MonoBehaviour {
     }
  
     void Update() {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit)) {
-            var selectionTransform = hit.transform;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Send a ray to the MIDDLE of the screen
+        RaycastHit hit; // information from the raycast
+        if (Physics.Raycast(ray, out hit)) { // check if the ray[cast] has hit a gameobject
+            var selectionTransform = hit.transform; // transform component of the object that the ray hit
  
-            if (selectionTransform.GetComponent<InteractableObject>()) {
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName(); 
-                interaction_Info_UI.SetActive(true);
-            } else  {
+            if (selectionTransform.GetComponent<InteractableObject>()) { // Check if the rayhit is something that should be interacted with (e.g. sky shouldn't be interactable by default)
+                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName(); // fill the text with the item name
+                interaction_Info_UI.SetActive(true); // display the item text to the scene
+            } else  { // do not display the text if the raycast hit is true but the item is not interactable 
                 interaction_Info_UI.SetActive(false);
             }
         }
