29b40ef8fd0f425fe3d99142f43d2c8563db0c37
Liam Arbuckle <liam@skinetics.tech>
2022-09-16 02:15:33 +0800

    📸🔭 -> Adding orbiter script for [SCD-1]

---
+++ Assets/Aliens/OpenGL-Mechanics/Orbiters.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiters : MonoBehaviour {
+    public int sphereCount = 500;
+    public int maxRadius = 200;
+    public GameObject[] spheres;
+    public Material[] mats;
+    public Material trailMat;
+
+    void Awake() {
+        spheres = new GameObject[sphereCount];
+    }
+
+    void Start() {
+        spheres = CreateSpheres(sphereCount, maxRadius);
+    }
+}
