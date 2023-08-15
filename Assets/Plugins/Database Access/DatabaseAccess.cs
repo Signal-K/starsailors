bcc21bf3b3372f24e822ce2d96d7804aa20c46c7
Liam Arbuckle <liam@skinetics.tech>
2023-08-15 14:28:35 +0800

    🦃🫕 ↝ Unity is now out of safemode :)

--- Assets/Plugins/Database Access/DatabaseAccess.cs
+++ Assets/Plugins/Database Access/DatabaseAccess.cs
@@ -3,5 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DatabaseAccess : MonoBehaviour {
-    MongoClient client = new MongoClient();
+    // MongoClient client = new MongoClient();
+    public int testInt = 1;
 }
