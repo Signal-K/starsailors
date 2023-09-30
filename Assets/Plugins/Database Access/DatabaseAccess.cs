dbf6b48f3747ac47b73485149bf6a869a88b3764
Liam Arbuckle <liam@skinetics.tech>
2023-09-30 18:46:41 +1000

    🚵🏻🐈‍⬛ ↝ Removing old scene content that's unneeded, see Notion issues

--- Assets/Plugins/Database Access/DatabaseAccess.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DatabaseAccess : MonoBehaviour {
-    // MongoClient client = new MongoClient();
-    public int testInt = 1;
-}
